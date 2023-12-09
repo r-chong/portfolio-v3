332f708697f8696fad9b37f387c34964b60a20d3
Reese Chong <dev.reese.chong@gmail.com>
2023-12-09 17:12:58 -0500

    Remove className on Header

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 				{/* dark:bg-[#946263] */}
 				<div className='bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] '></div>
 				{/* dark:bg-[#676394] */}
-				<Header className='' />
+				<Header />
 				{children}
 			</body>
 		</html>
